db2e2e7d0a1cf1e884a38cf6644ca60ec51f98d9
syf <15659142810@139.com>
2020-03-08 12:27:37 +0800

    commit by syf 2019-12-26

--- 自定义模块/show_time.py
+++ 自定义模块/show_time.py
@@ -1,47 +1,67 @@
 # -*- coding: utf-8 -*-
-# @Time: 2020/2/10 14:43
+# @Time: 2020/3/8 11:16
 import warnings
 warnings.filterwarnings(action="ignore")
 
-import datetime
 import time
+import datetime
 
 class show_time():
-    _start = time.time()
-    _start_ori = time.time()
+    __start = time.time()
+    __start_ori = time.time()
+
+    @staticmethod
+    def _now():
+        print("Time Now:", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), flush=True)
+
+    @staticmethod
+    def _diff_value(m, n):
+        spend_s = round(m/60, 2)
+        spend_m = round(spend_s/60, 2)
+        spend_h = round(spend_m/60, 2)
+        return (spend_s, spend_m, spend_h)
+
     def __init__(self):
-        ...
+        self._now()
 
-    def _now(self, describe="The Now"):
-        print("{}:".format(describe), datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), flush=True)
+    def reset_start(self):
+        self.__start = time.time()
+        return self.__start
 
-    def _restart(self):
-        self._start = time.time()
-
-    def _spend_time(self, module_name=''):
+    def module_spend_time(self, module_name=''):
         end = time.time()
-        spend_s = round(end - self._start, 2)
-        spend_m = round(spend_s / 60, 2)
-        spend_h = round(spend_m / 60, 2)
-        print("{} spend time: {} s = {} m = {} h".format(module_name, spend_s, spend_m, spend_h), flush=True)
+        s_ = end - self.__start
+        self._spend = self._diff_value(s_, 2)
+        self.__str__(module_name)
+        self._now()
+        return self._spend
 
-    def _all_spend_time(self):
+    def all_spend_time(self, module_name='all'):
         end = time.time()
-        spend_s = round(end - self._start_ori, 2)
-        spend_m = round(spend_s / 60, 2)
-        spend_h = round(spend_m / 60, 2)
-        print("All spend time: {} s = {} m = {} h".format(spend_s, spend_m, spend_h), flush=True)
+        s_ = end - self.__start_ori
+        self._spend = self._diff_value(s_, 2)
+        self.__str__(module_name)
+        self._now()
+        return self._spend
 
+    def __str__(self, module_name):
+        print("\t {} spend time: {} s = {} m = {} h".format(
+                    module_name, self._spend[0], self._spend[1], self._spend[2]), flush=True)
 
 if __name__ == "__main__":
-    time_class = show_time()
-    time_class._now()
-    time.sleep(5)
-    time_class._spend_time()
-    time_class._restart()
+    show_time = show_time()
     time.sleep(3)
-    time_class._spend_time()
-    time_class._all_spend_time()
-    time_class._now()
+    show_time.module_spend_time("module 1")
+    show_time.reset_start()
+    time.sleep(5)
+    show_time.module_spend_time("module 2")
+
+    show_time.all_spend_time("all")
+
+
+
+
+
+
 
 
