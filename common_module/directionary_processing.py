eb040dbd8cbd401fdaecb2c76607a6beb8c6ca24
shangyf <52403422+syf418@users.noreply.github.com>
2020-04-26 13:07:48 +0800

    commit by syf 2020-04-13

--- common_module/directionary_processing.py
+++ common_module/directionary_processing.py
@@ -5,19 +5,29 @@ warnings.filterwarnings(action='ignore')
 
 import os
 import shutil
-import traceback
 
-from common_project.common_module.logging_module import logging_func
-logger = logging_func()
 
 def makeDirs(path):
     if not os.path.exists(path):
         os.makedirs(path)
+
+def del_file(filepath):
+    """
+    删除某一目录下的所有文件或文件夹
+    :param filepath: 路径
+    :return:
+    """
+    del_list = os.listdir(filepath)
+    for f in del_list:
+        file_path = os.path.join(filepath, f)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
 
 def deleteDirs(dirs):
     try:
         if os.path.exists(dirs):
             shutil.rmtree(dirs)
     except Exception as e:
-        logger.exception(str(traceback.print_exc()))
         print("CLEARN Error %s" % (e), flush=True)
